27bbeedff71c297389183c2bea8f752112f42994
unknown <shreyascharli3489@gmail.com>
2026-04-11 13:35:21 +0530

    Adding config serve

---
+++ configserver/src/test/java/com/jobApplication/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobApplication.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
